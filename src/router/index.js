42b2686932e540b122962d0a367a0dd132cc5549
Mert Doruk Kalkan <mertdorukkalkan@gmail.com>
2024-04-20 12:38:11 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -11,5 +11,10 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 })
+router.onError((error, to) => {
+  if (error.message.includes('Failed to fetch dynamically imported module')) {
+    window.location = to.fullPath
+  }
+})
 
 export default router
